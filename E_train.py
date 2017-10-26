db099c3f15fd32b6661efecbc57734d9c25aa696
Donglai Wei <donglai@coxgpu06.rc.fas.harvard.edu>
2017-10-26 11:05:14 -0400

    refactor the logic for upsample

--- E_train.py
+++ E_train.py
@@ -165,12 +165,13 @@ def get_model(args, model_io_size):
 def get_optimizer(args, model, pre_epoch=0):
     betas = [float(x) for x in args.betas.split(',')]
     frozen_id = []
-    if args.num_gpu==1 and model.up[0].opt[0]==0: # hacked upsampling layer
-        for i in range(len(model.up)):
-            frozen_id +=  list(map(id,model.up[i].up._modules['0'].parameters()))
-    elif model.module.up[0].opt[0]==0: # hacked upsampling layer
-        for i in range(len(model.module.up)):
-            frozen_id +=  list(map(id,model.module.up[i].up._modules['0'].parameters()))
+    if model.up[0].opt[0]==0: # hacked upsampling layer
+        if args.num_gpu==1:
+            for i in range(len(model.up)):
+                frozen_id +=  list(map(id,model.up[i].up._modules['0'].parameters()))
+        else:
+            for i in range(len(model.module.up)):
+                frozen_id +=  list(map(id,model.module.up[i].up._modules['0'].parameters()))
     frozen_params = filter(lambda p: id(p) in frozen_id, model.parameters())
     rest_params = filter(lambda p: id(p) not in frozen_id, model.parameters())
     optimizer = torch.optim.Adam([
