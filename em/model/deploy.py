1de52f7ff3efbcd0868a6ead5d4196f048e363b2
Zudi Lin <linzudi@g.harvard.edu>
2018-02-27 16:46:37 -0500

    Update deploy.py

--- em/model/deploy.py
+++ em/model/deploy.py
@@ -302,7 +302,8 @@ class unet_m3_BasicBlock(nn.Module):
 
 #--------------------
 # CNN for VI-approx 
-# follow basic res-net architectures
+# follow basic res-net architectures, adapted from:
+# https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 
 def conv3_3D(in_planes, out_planes, stride=1):
     """3x3x3 convolution with padding"""
@@ -388,31 +389,31 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv3d(6, self.inplanes, kernel_size=5, stride=1, padding=2,
                                bias=False)
-        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.bn1 = nn.BatchNorm3d(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool3d((2,2,2), (2,2,2)
         self.layer1 = self._make_layer(block, self.inplanes*2, layers[0], stride=1)
-        self.layer2 = self._make_layer(block, self.inplanes*4, layers[1], stride=1)
-        self.layer3 = self._make_layer(block, self.inplanes*8, layers[2], stride=1)
+        self.layer2 = self._make_layer(block, self.inplanes*4, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, self.inplanes*8, layers[2], stride=2)
         self.avgpool = nn.AvgPool3d(7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(self.inplanes*8 * block.expansion, num_classes)
         self.relu = nn.ReLU(inplace=True)                            
 
         for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            if isinstance(m, nn.Conv3d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.kernel_size[2] * m.out_channels
                 m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
+            elif isinstance(m, nn.BatchNorm3d):
+                m.weight.data.fill_(1.0)
                 m.bias.data.zero_()
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
+                nn.Conv3d(self.inplanes, planes * block.expansion,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
+                nn.BatchNorm3d(planes * block.expansion),
             )
 
         layers = []
@@ -432,22 +433,16 @@ class ResNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = self.layer4(x)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
-
+        x = self.relu(x)
+                                    
         return x
 
                                     
-def cnn2_v1(pretrained=False, **kwargs):
-    """Constructs a ResNet-18 model.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+def cnn2_v1():
+    # Constructs a ResNet model to approximate VI.
+    model = ResNet(BasicBlock, [3, 6, 3], **kwargs)
     return model
