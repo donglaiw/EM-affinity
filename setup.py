b36d4078483495a4db42a03028cbdf0aeb33aa41
Zudi Lin <linzudi@g.harvard.edu>
2018-03-05 15:32:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,17 +22,17 @@ def setup_cython():
 	
     # utils for vi calculation
     ext_modules += [Extension(name='em.transforms.distance',
-        	    sources=['distance.pyx', 'cpp-distance.cpp'],
+        	    sources=['em/transforms/distance.pyx', 'em/transforms/cpp-distance.cpp'],
                     extra_compile_args=['-O4', '-std=c++0x'],
         	    language='c++')]
 
     ext_modules += [Extension(name='em.transforms.seg2gold',
-        	    sources=['seg2gold.pyx', 'cpp-seg2gold.cpp'],
+        	    sources=['em/transforms/seg2gold.pyx', 'em/transforms/cpp-seg2gold.cpp'],
                     extra_compile_args=['-O4', '-std=c++0x'],
                     language='c++')]
 
     ext_modules += [Extension(name='em.transforms.seg2seg',
-                    sources=['seg2seg.pyx', 'cpp-seg2seg.cpp'],
+                    sources=['em/transforms/seg2seg.pyx', 'em/transforms/cpp-seg2seg.cpp'],
                     extra_compile_args=['-O4', '-std=c++0x'],
                     language='c++')]
 	
