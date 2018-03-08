1d6622f9422d2a83f2d1032fda3249eb68887b2b
Zudi Lin <linzudi@g.harvard.edu>
2018-03-08 15:51:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ def setup_cython():
     from distutils.core import setup
     from distutils.extension import Extension
     from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
+    #from Cython.Build import cythonize
 	
     import numpy
     ext_modules = []
