66379c0fdc4e2e05e9ecc781f96815461c0b6fba
donglaiw <donglai@coxgpu06.rc.fas.harvard.edu>
2018-03-19 18:46:16 -0400

    update malis

---
+++ em_pth/model/deploy.py
@@ -0,0 +1,218 @@
+# deployed model without much flexibility
+# useful for stand-alone test, model translation, quantization
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .block import mergeAdd, mergeCrop
+
+
+class unet3D_m1(nn.Module): # deployed Toufiq model
+    def __init__(self, in_num=1, out_num=3, filters=[24,72,216,648],relu_slope=0.005, rescale_skip=0):
+        super(unet3D_m1, self).__init__()
+        self.filters = filters 
+        self.io_num = [in_num, out_num]
+        self.relu_slope = relu_slope
+
+        filters_in = [in_num] + filters[:-1]
+        self.depth = len(filters)-1
+        self.seq_num = self.depth*3+2
+
+        self.downC = nn.ModuleList([nn.Sequential(
+                nn.Conv3d(filters_in[x], filters_in[x+1], kernel_size=3, stride=1, bias=True),
+                nn.LeakyReLU(relu_slope),
+                nn.Conv3d(filters_in[x+1], filters_in[x+1], kernel_size=3, stride=1, bias=True),
+                nn.LeakyReLU(relu_slope))
+            for x in range(self.depth)]) 
+        self.downS = nn.ModuleList(
+                [nn.MaxPool3d((1,2,2), (1,2,2))
+            for x in range(self.depth)]) 
+        self.center = nn.Sequential(
+                nn.Conv3d(filters[-2], filters[-1], kernel_size=3, stride=1, bias=True),
+                nn.LeakyReLU(relu_slope),
+                nn.Conv3d(filters[-1], filters[-1], kernel_size=3, stride=1, bias=True),
+                nn.LeakyReLU(relu_slope))
+        self.upS = nn.ModuleList([nn.Sequential(
+                nn.ConvTranspose3d(filters[3-x], filters[3-x], (1,2,2), (1,2,2), groups=filters[3-x], bias=False),
+                nn.Conv3d(filters[3-x], filters[2-x], kernel_size=1, stride=1, bias=True))
+            for x in range(self.depth)]) 
+        # initialize upsample
+        for x in range(self.depth):
+            self.upS[x]._modules['0'].weight.data.fill_(1.0)
+
+        # double input channels: merge-crop
+        self.upC = nn.ModuleList([nn.Sequential(
+                nn.Conv3d(2*filters[2-x], filters[2-x], kernel_size=3, stride=1, bias=True),
+                nn.LeakyReLU(relu_slope),
+                nn.Conv3d(filters[2-x], filters[2-x], kernel_size=3, stride=1, bias=True),
+                nn.LeakyReLU(relu_slope))
+            for x in range(self.depth)]) 
+
+        self.final = nn.Sequential(nn.Conv3d(filters[0], out_num, kernel_size=1, stride=1, bias=True))
+
+
+    def getLearnableSeq(self, seq_id): # learnable variable 
+        if seq_id < self.depth:
+            return self.downC[seq_id]
+        elif seq_id == self.depth:
+            return self.center
+        elif seq_id != self.seq_num-1:
+            seq_id = seq_id-self.depth-1
+            if seq_id % 2 ==0:
+                return self.upS[seq_id/2]
+            else:
+                return self.upC[seq_id/2]
+        else:
+            return self.final
+
+    def setLearnableSeq(self, seq_id, seq): # learnable variable 
+        if seq_id < self.depth:
+            self.downC[seq_id] = seq
+        elif seq_id==self.depth:
+            self.center = seq
+        elif seq_id!=self.seq_num-1:
+            seq_id = seq_id-self.depth-1
+            if seq_id % 2 ==0:
+                self.upS[seq_id/2] = seq
+            else:
+                self.upC[seq_id/2] = seq
+        else:
+            self.final = seq
+
+    def forward(self, x):
+        down_u = [None]*self.depth
+        for i in range(self.depth):
+            down_u[i] = self.downC[i](x)
+            x = self.downS[i](down_u[i])
+        x = self.center(x)
+        for i in range(self.depth):
+            x = mergeCrop(down_u[self.depth-1-i], self.upS[i](x))
+            x = self.upC[i](x)
+        return F.sigmoid(self.final(x))
+
+def unet_m2_conv(in_num, out_num, kernel_size, pad_size, stride_size, has_bias, has_BN, relu_opt):
+    layers = []
+    for i in range(len(in_num)):
+        layers.append(nn.Conv3d(in_num[i], out_num[i], kernel_size=kernel_size[i], padding=pad_size[i], stride=stride_size[i], bias=has_bias[i])) 
+        if has_BN[i]:
+            layers.append(nn.BatchNorm3d(out_num[i]))
+        if relu_opt[i]==0:
+            layers.append(nn.ReLU(inplace=True))
+        elif relu_opt[i]==1:
+            layers.append(nn.ELU(inplace=True))
+    return nn.Sequential(*layers)
+
+class unet_m2_BasicBlock(nn.Module):
+    expansion = 1
+    def __init__(self, in_planes, out_planes, is_3D = True, has_BN = False, relu_opt = 0):
+        super(unet_m2_BasicBlock, self).__init__()
+        self.block1 = unet_m2_conv([in_planes], [out_planes], [(1,3,3)], [(0,1,1)], \
+                                   [1], [False], [has_BN], [relu_opt])
+        # no relu for the second block
+        if is_3D:
+            self.block2 = unet_m2_conv([out_planes]*2, [out_planes]*2, [(3,3,3)]*2, [(1,1,1)]*2, \
+                                       [1]*2, [False]*2, [has_BN]*2, [relu_opt,-1])
+        else: # a bit different due to bn-2D vs. bn-3D
+            self.block2 = unet_m2_conv([out_planes]*2, [out_planes]*2, [(1,3,3)]*2, [(0,1,1)]*2, \
+                                       [1]*2, [False]*2, [has_BN]*2, [relu_opt,-1])
+        if relu_opt==0:
+            self.block3 = nn.ReLU(inplace=True)
+        else:
+            self.block3 = nn.ELU(inplace=True)
+
+    def forward(self, x):
+        residual = self.block1(x)
+        out = residual + self.block2(residual)
+        out = self.block3(out)
+        return out
+
+class unet3D_m2(nn.Module): # deployed PNI model
+    # Superhuman Accuracy on the SNEMI3D Connectomics Challenge. Lee et al.
+    # https://arxiv.org/abs/1706.00120
+    def __init__(self, in_num=1, out_num=3, filters=[28,36,48,64,80], has_BN=True, relu_opt=0):
+        super(unet3D_m2, self).__init__()
+        self.filters = filters 
+        self.io_num = [in_num, out_num]
+        self.res_num = len(filters)-2
+        self.seq_num = (self.res_num+1)*2+1
+        
+        self.downC = nn.ModuleList(
+                [unet_m2_conv([in_num], [filters[0]], [(1,5,5)], [(0,2,2)], [1], [False], [has_BN], [relu_opt])]
+                + [unet_m2_BasicBlock(filters[x], filters[x+1], True, has_BN, relu_opt)
+                      for x in range(self.res_num)]) 
+        self.downS = nn.ModuleList(
+                [nn.MaxPool3d((1,2,2), (1,2,2))
+            for x in range(self.res_num+1)]) 
+        self.center = unet_m2_BasicBlock(filters[-2], filters[-1], True, has_BN, relu_opt)
+        self.upS = nn.ModuleList(
+            [nn.Sequential(
+                nn.ConvTranspose3d(filters[self.res_num+1-x], filters[self.res_num+1-x], (1,2,2), (1,2,2), groups=filters[self.res_num+1-x], bias=False),
+                nn.Conv3d(filters[self.res_num+1-x], filters[self.res_num-x], kernel_size=(1,1,1), stride=1, bias=True))
+                for x in range(self.res_num+1)]) 
+        # initialize upsample
+        for x in range(self.res_num+1):
+            self.upS[x]._modules['0'].weight.data.fill_(1.0)
+
+        self.upC = nn.ModuleList(
+            [unet_m2_BasicBlock(filters[self.res_num-x], filters[self.res_num-x], True, has_BN, relu_opt)
+                for x in range(self.res_num)]
+            + [nn.Conv3d(filters[0], out_num, kernel_size=(1,5,5), stride=1, padding=(0,2,2), bias=True)]) 
+
+    def forward(self, x):
+        down_u = [None]*(self.res_num+1)
+        for i in range(self.res_num+1):
+            down_u[i] = self.downC[i](x)
+            x = self.downS[i](down_u[i])
+        x = self.center(x)
+        for i in range(self.res_num+1):
+            x = down_u[self.res_num-i] + self.upS[i](x)
+            x = self.upC[i](x)
+        return F.sigmoid(x)
+
+class unet3D_m2_v2(nn.Module):
+    # changes from unet3D_m2
+    # - add 2D residual module
+    # - add 2D residual module
+    def __init__(self, in_num=1, out_num=3, filters=[28,36,48,64,80], has_BN=True, relu_opt=0):
+        super(unet3D_m2_v2, self).__init__()
+        self.filters = filters 
+        self.io_num = [in_num, out_num]
+        self.res_num = len(filters)-2
+        self.seq_num = (self.res_num+1)*2+1
+
+        self.downC = nn.ModuleList(
+                [unet_m2_conv([in_num], [1], [(1,5,5)], [(0,2,2)], [1], [False], [has_BN], [relu_opt])]
+                + [unet_m2_BasicBlock(1, filters[0], False, has_BN, relu_opt)]
+                + [unet_m2_BasicBlock(filters[x], filters[x+1], True, has_BN, relu_opt)
+                      for x in range(1, self.res_num)]) 
+        self.downS = nn.ModuleList(
+                [nn.MaxPool3d((1,2,2), (1,2,2))
+            for x in range(self.res_num+1)]) 
+        self.center = unet_m2_BasicBlock(filters[-2], filters[-1], True, has_BN, relu_opt)
+        self.upS = nn.ModuleList(
+            [nn.Sequential(
+                nn.ConvTranspose3d(filters[self.res_num+1-x], filters[self.res_num+1-x], (1,2,2), (1,2,2), groups=filters[self.res_num+1-x], bias=False),
+                nn.Conv3d(filters[self.res_num+1-x], filters[self.res_num-x], kernel_size=(1,1,1), stride=1, bias=True))
+                for x in range(self.res_num+1)]) 
+        # initialize upsample
+        for x in range(self.res_num+1):
+            self.upS[x]._modules['0'].weight.data.fill_(1.0)
+
+        self.upC = nn.ModuleList( # same number of channels from the left
+            [unet_m2_BasicBlock(filters[self.res_num-x], filters[self.res_num-x], True, has_BN, relu_opt)
+                for x in range(self.res_num-1)]
+            + [unet_m2_BasicBlock(filters[0], filters[0], False, has_BN, relu_opt)]
+            + [nn.Conv3d(filters[0], out_num, kernel_size=(1,5,5), stride=1, padding=(0,2,2), bias=True)]) 
+
+    def forward(self, x):
+        down_u = [None]*(self.res_num+1)
+        x = self.downC[0](x) # first 1x5x5
+        for i in range(self.res_num+1):
+            down_u[i] = self.downC[1+i](x)
+            x = self.downS[i](down_u[i])
+        x = self.center(x)
+        for i in range(self.res_num+1):
+            x = down_u[self.res_num-i] + self.upS[i](x)
+            x = self.upC[i](x)
+        x = self.upC[-1](x) # last 1x5x5
+        return F.sigmoid(x)
+
---
+++ em_pth/model/io.py
@@ -0,0 +1,227 @@
+import sys
+import numpy as np
+import h5py
+
+# 1. model i/o
+def save_checkpoint(model, filename='checkpoint.pth', optimizer=None, epoch=1):
+    # model or model_state
+    out = model if type(model) is dict else model.state_dict()
+         
+    import torch
+    if optimizer is None:
+        torch.save({
+            'epoch': epoch,
+            'state_dict': out,
+        }, filename)
+    else:
+        torch.save({
+            'epoch': epoch,
+            'state_dict': out,
+            'optimizer' : optimizer.state_dict()
+        }, filename)
+
+def convert_state_dict(state_dict, num_gpu):
+    # multi-single gpu conversion
+    if num_gpu==1 and state_dict.keys()[0][:7]=='module.':
+        # modify the saved model for single GPU
+        for k,v in state_dict.items():
+            state_dict[k[7:]] = state_dict.pop(k,None)
+    elif num_gpu>1 and (len(state_dict.keys()[0])<7 or state_dict.keys()[0][:7]!='module.'):
+        # modify the single gpu model for multi-GPU
+        for k,v in state_dict.items():
+            state_dict['module.'+k] = v
+            state_dict.pop(k,None)
+
+def load_checkpoint(snapshot, num_gpu=1):
+    import torch
+    if isinstance(snapshot, basestring):
+        cp = torch.load(snapshot)
+        if type(cp) is not dict:
+            # model -> state_dict
+            cp={'epoch':0, 'state_dict': cp.state_dict()}
+    else:
+        cp={'epoch':0, 'state_dict': snapshot}
+    convert_state_dict(cp['state_dict'], num_gpu)
+    return cp
+
+# 2. model conversion
+def keras2pkl(mn,outN=None):
+    pass
+def caffe2pkl(mn,wn,outN=None):
+    import caffe
+    net0 = caffe.Net(mn,caffe.TEST,weights=wn)
+    net={}
+    for k,v in net0.params.items():
+        layer={}
+        layer['w']=v[0].data
+        if len(v)>1: # deconv layer: no bias term
+            layer['b']=v[1].data
+        net[k]=layer
+    if outN is None:
+        return net
+    else:
+        import pickle
+        pickle.dump(net,open(outN,'wb'))
+
+def pth2issac(net):
+    import isaac.pytorch
+    # now only supports: 
+    result = isaac.pytorch.UNet(out_num=net.io_num[1], 
+                filters=[net.io_num[0]]+net.filters,
+                relu_slope=net.relu_slope).cuda()
+
+    # Reorder indices because new state dict has upsample-upconv interleaved
+    depth = net.depth
+    ndown = 4*(depth + 1)
+    reorder = list(range(ndown))
+    for i in range(depth):
+        upsamples = list(range(ndown + i*3, ndown + i*3 + 3))
+        upconvs = list(range(ndown + depth*3 + i*4, ndown + depth*3 + i*4 + 4))
+        reorder +=  upsamples + upconvs
+    reorder += [ndown + 7*depth, ndown + 7*depth + 1]
+
+    # Copy in proper order
+    net_keys = list(net.state_dict().keys())
+    result_keys = list(result.state_dict().keys())
+    net_dict = net.state_dict()
+    result_dict = result.state_dict()
+    for i, j in enumerate(reorder):
+        result_dict[result_keys[i]] = net_dict[net_keys[j]].clone()
+    result.load_state_dict(result_dict)
+
+    return result
+
+
+def pth2pkl(mn,outN=None):
+    import torch
+    net0 = torch.load(mn)
+    net={}
+    cc=1
+    bn=1
+    ks= net0['state_dict'].keys()
+    for k in ks:
+        if 'cbrd.0.weight' in k and 'up.0.weight' not in k: #no deconv
+            layer={}
+            print cc,k
+            layer['w']=net0['state_dict'][k].cpu().numpy()
+            kk=k.replace('weight','bias')
+            if kk in ks:
+                layer['b']=net0['state_dict'][kk].cpu().numpy()
+            net['Convolution'+str(cc)]=layer
+            cc+=1
+        elif 'cbrd.1.running_mean' in k: #batchnorm
+            layer={}
+            print bn,k
+            layer['w0']=net0['state_dict'][k.replace('running_mean','weight')].cpu().numpy()
+            layer['w1']=net0['state_dict'][k.replace('running_mean','bias')].cpu().numpy()
+            layer['w2']=net0['state_dict'][k].cpu().numpy()
+            layer['w3']=net0['state_dict'][k.replace('running_mean','running_var')].cpu().numpy()
+            net['BatchNorm'+str(bn)]=layer
+            bn+=1
+    if outN is None:
+        return net
+    else:
+        import pickle
+        pickle.dump(net,open(outN,'wb'))
+
+# 3. initialization
+def weight_filler(ksizes, opt_scale=2.0, opt_norm=2):
+    kk=0
+    if opt_norm==0:# n_in
+        kk = np.prod(ksizes[1:])
+    elif opt_norm==1:# n_out
+        kk = ksizes[0]*np.prod(ksizes[2:])
+    elif opt_norm==2:# (n_in+n_out)/2
+        kk = np.mean(ksizes[:2])*np.prod(ksizes[2:])
+    # opt_scale: 1.0=xavier, 2.0=kaiming, 3.0=caffe
+    ww = np.sqrt(opt_scale/float(kk))
+    return ww
+
+def init_weights(model,opt_init=0):
+    import torch
+    opt=[[2.0,2],[3.0,0]][opt_init]
+    for i in range(3):
+        for j in range(2):
+            ksz = model.down[i].conv.convs[j].cbrd._modules['0'].weight.size()
+            model.down[i].conv.conv.convs[j].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(np.random.normal(0, weight_filler(ksz,opt[0],opt[1]), ksz)))
+            model.down[i].conv.convs[j].cbrd._modules['0'].bias.data.fill_(0.0)
+    # center
+    for j in range(2):
+        ksz = model.center.convs[j].cbrd._modules['0'].weight.size()
+        model.center.convs[j].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(np.random.normal(0, weight_filler(ksz,opt[0],opt[1]), ksz)))
+        model.center.convs[j].cbrd._modules['0'].bias.data.fill_(0.0)
+    # up
+    for i in range(3):
+        model.up[i].up.weight.data.fill_(1.0)
+        ksz = model.up[i].up_conv.convs[0].cbrd._modules['0'].weight.size()
+        model.up[i].up_conv.convs[0].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(np.random.normal(0, weight_filler(ksz,opt[0],opt[1]), ksz)))
+        model.up[i].up_conv.convs[0].cbrd._modules['0'].bias.data.fill_(0.0)
+        for j in range(2):
+            ksz = model.up[i].conv.convs[j].cbrd._modules['0'].weight.size()
+            model.up[i].conv.convs[j].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(np.random.normal(0, weight_filler(ksz,opt[0],opt[1]), ksz)))
+            model.up[i].conv.convs[j].cbrd._modules['0'].bias.data.fill_(0.0)
+    ksz = model.final.conv.weight.size()
+    model.final.convs[0].cbrd.weight.data.copy_(torch.from_numpy(np.random.normal(0, weight_filler(ksz,opt[0],opt[1]), ksz)))
+    model.final.convs[0].cbrd.bias.data.fill_(0.0)
+
+def load_weights_pkl(model, weights):
+    import torch
+    # todo: rewrite for more flexibility
+    # todo: add BN param: running mean
+    # down
+    for i in range(3):
+        for j in range(2):
+            ww = weights['Convolution'+str(2*i+j+1)]
+            model.down[i].conv[j].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+            model.down[i].conv[j].cbrd._modules['0'].bias.data.copy_(torch.from_numpy(ww['b']))
+    # center
+    for j in range(2):
+        ww = weights['Convolution'+str(7+j)]
+        model.center.conv[j].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+        model.center.conv[j].cbrd._modules['0'].bias.data.copy_(torch.from_numpy(ww['b']))
+    # up
+    for i in range(3):
+        ww = weights['Deconvolution'+str(i+1)]
+        if model.up[i].opt[0]==0:# upsample+conv
+            model.up[i].up._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+            ww = weights['Convolution'+str(9+i*3)]
+            model.up[i].up._modules['1'].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+            model.up[i].up._modules['1'].cbrd._modules['0'].bias.data.copy_(torch.from_numpy(ww['b']))
+        elif model.up[i].opt[0]==1:# deconv
+            model.up[i].up.weight.data.copy_(torch.from_numpy(ww['w']))
+        for j in range(2):
+            ww = weights['Convolution'+str(10+3*i+j)]
+            model.up[i].conv[j].cbrd._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+            model.up[i].conv[j].cbrd._modules['0'].bias.data.copy_(torch.from_numpy(ww['b']))
+    ww = weights['Convolution18']
+    model.final.conv.cbrd._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+    model.final.conv.cbrd._modules['0'].bias.data.copy_(torch.from_numpy(ww['b']))
+
+def load_weights_pkl_m1(model, weights):
+    import torch
+    # down
+    for i in range(3):
+        for j in range(2):
+            ww = weights['Convolution'+str(2*i+j+1)]
+            model.downC[i]._modules[str(j*2)].weight.data.copy_(torch.from_numpy(ww['w']))
+            model.downC[i]._modules[str(j*2)].bias.data.copy_(torch.from_numpy(ww['b']))
+    # center
+    for j in range(2):
+        ww = weights['Convolution'+str(7+j)]
+        model.center._modules[str(j*2)].weight.data.copy_(torch.from_numpy(ww['w']))
+        model.center._modules[str(j*2)].bias.data.copy_(torch.from_numpy(ww['b']))
+    # up
+    for i in range(3):
+        ww = weights['Deconvolution'+str(i+1)]
+        model.upS[i]._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+        ww = weights['Convolution'+str(9+i*3)]
+        model.upS[i]._modules['1'].weight.data.copy_(torch.from_numpy(ww['w']))
+        model.upS[i]._modules['1'].bias.data.copy_(torch.from_numpy(ww['b']))
+        for j in range(2):
+            ww = weights['Convolution'+str(10+3*i+j)]
+            model.upC[i]._modules[str(2*j)].weight.data.copy_(torch.from_numpy(ww['w']))
+            model.upC[i]._modules[str(2*j)].bias.data.copy_(torch.from_numpy(ww['b']))
+    ww = weights['Convolution18']
+    model.final._modules['0'].weight.data.copy_(torch.from_numpy(ww['w']))
+    model.final._modules['0'].bias.data.copy_(torch.from_numpy(ww['b']))
+
